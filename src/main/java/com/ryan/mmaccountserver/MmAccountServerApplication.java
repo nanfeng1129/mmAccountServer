f461324ab94ac079311c93eb4e36db7d195ca4a3
Ryan <1594741693@qq.com>
2022-09-16 11:25:22 +0800

    Initial commit

---
+++ src/main/java/com/ryan/mmaccountserver/MmAccountServerApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.mmaccountserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmAccountServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmAccountServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryan/mmaccountserver/MmAccountServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.mmaccountserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmAccountServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
